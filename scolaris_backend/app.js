998d77a051312969cecfdac5320a14597da566be
MChen407 <asmourchid@gmail.com>
2025-08-18 11:03:55 +0100

    feat: mise en place du routage pour les élèves

--- scolaris_backend/app.js
+++ scolaris_backend/app.js
@@ -1,11 +1,10 @@
- const express = require('express')
+import studentRoutes from "./src/routes/student.route.js";
+
+const express = require('express')
 const app = express()
 
-app.use(express.json())
+app.use("/api/students", studentRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API Scolaris opérationnelle')
-})
 
 app.listen(3000, () => {
   console.log('Serveur démarré sur le port 3000')
